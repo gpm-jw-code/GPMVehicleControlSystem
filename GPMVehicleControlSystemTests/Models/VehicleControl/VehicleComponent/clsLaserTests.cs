71349e221217f33ed9219c027c6b0130321707a7
jinweihuang <jinweihuang2033@outlook.com>
2023-05-26 09:40:22 +0800

    單元測試修正

---
+++ GPMVehicleControlSystemTests/Models/VehicleControl/VehicleComponent/clsLaserTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPMVehicleControlSystem.Models.VehicleControl.VehicleComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMVehicleControlSystem.Models.VehicleControl.VehicleComponent.Tests
+{
+    [TestClass()]
+    public class clsLaserTests
+    {
+        [TestMethod()]
+        public void GetLaserINTest()
+        {
+        }
+    }
+}
